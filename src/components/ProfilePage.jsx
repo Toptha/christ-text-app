ee87f3d26a4b2fc266bbe6a1df1794e4b11023e2
Preetham Aacharya <149096832+Toptha@users.noreply.github.com>
2025-05-18 14:35:30 +0530

    idek wtf im doing

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -2,7 +2,7 @@ import './styles/profile.css';
 import logo from '../assets/logo.png';
 import { useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-import jwt_decode from 'jwt-decode'; 
+import { jwt_decode } from 'jwt-decode'; 
 
 function ProfilePage() {
   const navigate = useNavigate();
